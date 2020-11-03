3ef3b2d2346db1066cc6603599116956d32f8489
30111971 <matheusmaica@gmail.com>
2020-11-02 21:05:51 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+require("dotenv").config()
+app.use(express.static(path.join(__dirname, "./public")));
+app.get("*", async (req, res) => res.sendFile(__dirname + "/homepage.hmtl"))
+app.listen(process.env.PORT)
